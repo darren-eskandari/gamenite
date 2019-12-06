62590d6cb44a6a73944d878d6c35973f8171a837
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-12-06 15:45:19 -0800

    remove app.test.js

---
+++ gamenite/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+import { firebase } from './firebase/firebase'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Switch>
+        <Route  />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
