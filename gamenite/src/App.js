f7f91a21fa4c82783a0ec09b5a17ea2d853f606b
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-12-09 11:46:56 -0800

    route NavBar links through app.js

--- gamenite/src/App.js
+++ gamenite/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import NavBar from './components/NavBar'
 
 
 import * as ROUTES from './constants/routes';
@@ -22,10 +23,13 @@ class App extends Component {
 
     return (
       <div className="App">
+        <NavBar />
 
         <h1>Hello World</h1>
         <Switch>
           <Route exact path={ROUTES.HOME} render={() => <div>home</div>} />
+          <Route exact path={ROUTES.LOGIN} render={() => <div>login</div>} />
+          <Route exact path={ROUTES.SIGN_UP} render={() => <div>sign up</div>} />
         </Switch>
       </div>
     );
