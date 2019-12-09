ff51bcb1c7f3980440c70f50e71092d20011faf4
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-12-09 11:39:25 -0800

    set up app.js as class

--- gamenite/src/App.js
+++ gamenite/src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import { firebase } from './firebase/firebase'
 
+
+import * as ROUTES from './constants/routes';
+import { firebase } from './firebase/firebase';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Switch>
-        <Route  />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    message: '',
+    currentUser: null,
+    profilePic: null
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App">
+
+        <h1>Hello World</h1>
+        <Switch>
+          <Route exact path={ROUTES.HOME} render={() => <div>home</div>} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
