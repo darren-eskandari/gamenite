5fe5a996ec279da8a2d82d74b2dcaf53b9bc07fb
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-12-09 22:28:09 -0800

    Build Game Show component

--- gamenite/src/components/ShowGame/index.js
+++ gamenite/src/components/ShowGame/index.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class ShowGame extends Component {
 
     state = {
-        name: ""
+        name: "",
+        img: "",
+        minPlayers: "",
+        maxPlayers: "",
+        minPlaytime: "",
+        maxPlaytime: "",
+        description: "",
+        description_preview: "",
     }
 
     async componentDidMount() {
@@ -14,13 +21,32 @@ class ShowGame extends Component {
 
         const gameJson = await resGame.json()
         console.log(gameJson)
+        // console.log(gameJson.games[0].name)
+
+        this.setState({
+            name: gameJson.games[0].name,
+            img: gameJson.games[0].images.medium,
+            minPlayers: gameJson.games[0].min_players,
+            maxPlayers: gameJson.games[0].max_players,
+            minPlaytime: gameJson.games[0].min_playtime,
+            maxPlaytime: gameJson.games[0].max_playtime,
+            description: gameJson.games[0].description,
+            description_preview: gameJson.games[0].description_preview,
+        })
     }
 
 
     render() {
+        console.log(this.state.name)
         return (
             <div className="show-game">
-                Show Game
+                <h3>{this.state.name}</h3>
+                <img src={this.state.img} />
+                Players: {this.state.minPlayers}-{this.state.maxPlayers}
+                Average Playtime: {this.state.minPlaytime}-{this.state.maxPlaytime}
+                <p>
+                    {this.state.description_preview}
+                </p>
             </div>
         )
     }
